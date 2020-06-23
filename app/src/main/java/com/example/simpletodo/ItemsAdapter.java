8ebbda90ff02f6ff61f877f8b229002044fc4083
Saikou Ceesay <saikz@fb.com>
2020-06-23 15:08:54 -0700

    itemsAdapter class created and the recycle view working properly with pre-defined todos

---
+++ app/src/main/java/com/example/simpletodo/ItemsAdapter.java
@@ -0,0 +1,55 @@
+package com.example.simpletodo;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+//responsible for displaying data from the model into a row in the recycler view
+public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ViewHolder>{
+    List<String> items;
+    public ItemsAdapter(List<String> items) {
+        this.items = items;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        //use layout inflator to inflate a view
+        View todoView = LayoutInflater.from(parent.getContext()).inflate(android.R.layout.simple_list_item_1, parent, false);
+        //wrap it inside a view holder and return it
+        return new ViewHolder(todoView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        //grab the item at the position
+        String item = items.get(position);
+        //bind the item into the specified view holder
+        holder.bind(item);
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    //container to provide easy access to views that represent each row of the list
+    class ViewHolder extends RecyclerView.ViewHolder{
+        TextView tvItem;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tvItem = itemView.findViewById(android.R.id.text1);
+        }
+        //update the view inside of the view holder with this data
+        public void bind(String item) {
+            tvItem.setText(item);
+        }
+    }
+}
--- app/src/main/java/com/example/simpletodo/MainActivity.java
+++ app/src/main/java/com/example/simpletodo/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.simpletodo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    List<String> items;
+    Button btnAdd;
+    RecyclerView rvItems;
+    EditText edtItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnAdd = findViewById(R.id.btnAdd);
+        rvItems = findViewById(R.id.rvItems);
+        edtItem = findViewById(R.id.edtItem);
+
+        items = new ArrayList<String>();
+        items.add("milk");
+        items.add("butter");
+
+        ItemsAdapter itemsAdapter = new ItemsAdapter(items);
+        rvItems.setAdapter(itemsAdapter);
+        rvItems.setLayoutManager(new LinearLayoutManager(this));
+
+
     }
 }
